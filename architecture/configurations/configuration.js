46201f5fb028e96194de98ebae336302f54c46a7
Robert-Adrian <57811734+Robert-Adrian@users.noreply.github.com>
2021-08-09 17:34:22 +0300

    Add configuration information to connect to clearDB

--- architecture/configurations/configuration.js
+++ architecture/configurations/configuration.js
@@ -3,11 +3,11 @@ const app = express();
 const mysql = require('mysql');
 const port = 8000;
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "us-cdbr-east-04.cleardb.com",
     port: 3306,
-    user: "root",
-    database: "usermanager",
-    password: ""
+    user: "b5566eaa1acbbc",
+    database: "heroku_8f4e7a97300436f",
+    password: "b58c37ab"
 });
 const router = express.Router();
 
